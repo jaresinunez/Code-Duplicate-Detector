dd600481532716c4d23ebe07064a09dca4a52afc
kgonz038 <kgonzalez1718@gmail.com>
2022-10-12 21:40:57 -0500

    Save files info text as a string
    Added the app to read and save files submitted as strings along with additional labels and text areas to display the path of the file submitted and the file's contents.

--- CodeDuplicateDetector/src/CodeDuplicateDetector.java
+++ CodeDuplicateDetector/src/CodeDuplicateDetector.java
@@ -1,11 +1,12 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
 /**
  *
@@ -31,13 +32,17 @@ public class CodeDuplicateDetector extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         button1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         setResizable(false);
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Please Submit a java source file");
+        jLabel1.setText("Please submit a file below.");
         jLabel1.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
 
         button1.setText("Submit Files");
@@ -47,28 +52,45 @@ public class CodeDuplicateDetector extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jTextArea2.setEditable(false);
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(403, 403, 403)
+                            .addComponent(button1)
+                            .addGap(80, 80, 80)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(246, 246, 246)
-                        .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(186, 186, 186))
+                        .addGap(87, 87, 87)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane2)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 744, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(96, 96, 96))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(122, Short.MAX_VALUE)
+                .addGap(31, 31, 31)
                 .addComponent(jLabel1)
-                .addGap(39, 39, 39)
-                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(61, 61, 61))
+                .addGap(18, 18, 18)
+                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
@@ -76,26 +98,51 @@ public class CodeDuplicateDetector extends javax.swing.JFrame {
 
     private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
                 
-        
         if (evt.getSource()==button1)
         {
             JFileChooser submit_file = new JFileChooser();
             
-           // submit_file.showOpenDialog(null);
-           
            int response = submit_file.showOpenDialog(null);
-           int responseSave = submit_file.showSaveDialog(null);
+           //int responseSave = submit_file.showSaveDialog(null);
            
-           if(responseSave == JFileChooser.APPROVE_OPTION)
+           if(response == JFileChooser.APPROVE_OPTION)
            {
                File path = new File(submit_file.getSelectedFile().getAbsolutePath());
-               System.out.println(path);
+               
+        FileReader fr = null; 
+                try {
+                    fr = new FileReader(path);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(CodeDuplicateDetector.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        
+        String fileData = "";
+        
+        int c; 
+                try {
+                    while((c=fr.read()) != -1) 
+                        fileData += (char)c;
+                } catch (IOException ex) {
+                    Logger.getLogger(CodeDuplicateDetector.class.getName()).log(Level.SEVERE, null, ex);
+                }
+               System.out.println("Opening source file from the path-> "+path);
+               System.out.println(fileData);
+    
+               
                jLabel1.setText("File submitted!");
+               jLabel2.setText("Opening  " + path);
+               jTextArea2.setText(fileData);
            }
 
         }
     }//GEN-LAST:event_button1ActionPerformed
 
+    private void stringDuplicatePercent()
+    {
+        
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -134,5 +181,8 @@ public class CodeDuplicateDetector extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton button1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea2;
     // End of variables declaration//GEN-END:variables
 }
